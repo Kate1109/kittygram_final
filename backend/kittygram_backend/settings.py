a391a1ca317d543b26d309ffcbf34366aa0af0c4
Екатерина Тачилина <loginTEI@yandex.ru>
2024-05-31 11:47:12 +0300

    Ревью4

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['infratei.ru']
+ALLOWED_HOSTS = ['infratei.ru', '158.160.14.208', '127.0.0.1', 'localhost']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
